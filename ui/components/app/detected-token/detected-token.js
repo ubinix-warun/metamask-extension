8ffd9669f70d34784ad9bed98bd0b5e0dd5097c3
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-04-14 20:09:15 -0400

    updating the state value

--- ui/components/app/detected-token/detected-token.js
+++ ui/components/app/detected-token/detected-token.js
@@ -82,8 +82,8 @@ const DetectedToken = ({ setShowDetectedTokens }) => {
     if (selectedTokens.length < detectedTokens.length) {
       setShowDetectedTokenIgnoredPopover(true);
     } else {
-      await dispatch(importTokens(selectedTokens));
-      const tokenSymbols = selectedTokens.map(({ symbol }) => symbol);
+      const tokenSymbols = selectedTokensList.map(({ symbol }) => symbol);
+      await dispatch(importTokens(selectedTokensList));
       dispatch(setNewTokensImported(tokenSymbols.join(', ')));
       setShowDetectedTokens(false);
     }
